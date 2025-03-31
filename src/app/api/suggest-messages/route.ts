6db6a8f1a78d79b3c3e69811b99c2edb7df117a2
Sattwik Bhakta <sattwikbhakta@gmail.com>
2025-03-31 11:38:05 +0530

    feat(public profile page): fetch suggested messages for public profile

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -4,23 +4,16 @@ const groq = new Groq({
   apiKey: process.env.GROQ_AI_KEY,
 });
 
-export async function POST(req: Request): Promise<Response> {
+export async function GET(): Promise<Response> {
   try {
-    // Parse the request body to extract messages
-    const body = await req.json();
-    const { messages } = body;
-
-    if (!Array.isArray(messages)) {
-      return new Response(
-        JSON.stringify({ error: 'Invalid request: "messages" must be an array of objects.' }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
     // Create a stream from the Groq API
     const stream = await groq.chat.completions.create({
-      messages:[
-        { role: 'system', content: "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."},
+      messages: [
+        {
+          role: "system",
+          content:
+            "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.",
+        },
       ],
       model: "llama-3.3-70b-versatile",
       temperature: 0.5,
@@ -70,5 +63,3 @@ export async function POST(req: Request): Promise<Response> {
     );
   }
 }
-
-
